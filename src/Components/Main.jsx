869c00f825ec78bf37800460b6a31905bca2f947
Daegyun Lim <dev.peter.lim@gmail.com>
2022-06-10 15:30:47 +0900

    working on Main.js, postReducer.js files

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,13 +1,32 @@
 /* 비로그인/로그인 상태 모두에서 보여지는 Component, Home.jsx 페이지에 렌더링 됨
 */
+import React from "react";
+import { useSelector } from "react-redux";
+
+export const PostItem = () => {
+  return (
+    <div>
+    </div>
+  )
+}
 
 const Main = () => {
-
-
-
+ const postList = useSelector(state=>state.postReducer.result)
+console.log(postList)
   return (
     <>
-
+      {postList.map((v, i) => {
+        return (
+        <PostItem
+          img_url = {v.img}
+          nickname = {v.nickname}
+          postId = {v.postId}
+          userId = {v.userId}
+          index = {i}
+          key = {v.postId}
+        ></PostItem>
+        )
+      })}
     </>
   );
 }
--- src/redux/modules/postReducer.jsx
+++ src/redux/modules/postReducer.jsx
@@ -1,2 +1,43 @@
 /* CRUD reducer part */
 
+// Init State
+const initState = {
+    ok:true,
+    result: [{
+        img:"randomImg for test",
+        nickname:"Nicks",
+        postId:"asdfasdfsadfas",
+        userId:"Peterk"
+    }],
+}
+
+//Action Type
+const ADD_POST = 'postReducer/ADD_POST';
+const LOAD_POST = 'postReducer/LOAD_POST';
+const UPDATE_POST = 'postReducer/UPDATE_POST';
+const DELETE_POST = 'postReducer/DELETE_POST';
+
+//Action Creator 
+function addPost (payload) {
+    return {type: ADD_POST, payload}
+}
+function loadPost (payload) {
+    return {type: LOAD_POST, payload}
+}
+function updatePost (payload) {
+    return {type: UPDATE_POST, payload}
+}
+function deletePost (payload) {
+    return {type: DELETE_POST, payload}
+}
+
+export default function postReducer(state=initState, action={}){
+    switch (action.type){
+        case ADD_POST: {
+            return {...state, list: action.payload}
+        } case LOAD_POST: {
+            return {list: action.payload}
+        } default:
+            return state;
+    }
+}
