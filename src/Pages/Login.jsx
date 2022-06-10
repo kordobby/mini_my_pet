e819a85e7b9ec69d2368e509bb183b0a4a9f6731
kordobby <poordobby@naver.com>
2022-06-11 02:55:32 +0900

    add login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,15 +1,78 @@
 /* 로그인 page */
+import React, { useState } from 'react';
 
+/* Styles */
+import styled from 'styled-components';
+
+/* Routes */
+import { useNavigate } from 'react-router-dom';
+
+/* REDUX */
+import { useDispatch, useSelector } from 'react-redux';
+import { loginDB } from '../redux/modules/userReducer';
 
 const Login = () => {
 
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const request = useSelector((state) => state.userReducer);
+  console.log(request);
 
+  const [ id, setId ] = useState('');
+  const [ pw, setPw ] = useState('');
+
+  const loginHandler = () => {
+    dispatch(loginDB({
+      userId : id,
+      password : pw
+    }))
+    if ( request.login === true ) {
+      navigate('/')
+    } else if ( request.error ) {
+      setId(null);
+      setPw(null);
+    }
+  };
 
   return (
-    <>
-      <span>Login</span>
-    </>
+    <UserFormWrap>
+      <UserPageBox>
+        <UserTitle>LOGIN PAGE</UserTitle>
+        <InputBox 
+          type = "text"
+          onChange = {(event) => { setId(event.target.value); console.log(id);}}
+          placeholder = "ID를 입력해주세요."
+          required />
+        <InputBox
+          type = "password" 
+          onChange = {(event) => { setPw(event.target.value); console.log(pw);}} 
+          placeholder = "PW를 입력해주세요."
+          required />
+      </UserPageBox>
+      <Button onClick = {loginHandler} >로그인하기</Button>
+    </UserFormWrap>
   )
 }
 
+export const UserFormWrap = styled.div`
+  margin-top: 100px;
+  display : flex;
+  flex-direction: column;
+`
+export const UserPageBox = styled.div`
+  width : 500px;
+  display : flex;
+  flex-direction: column;
+`
+export const UserTitle = styled.div`
+
+`
+export const InputBox = styled.input`
+&::placeholder {
+  color : grey;
+}
+`
+export const Button = styled.button`
+  width : 500px;
+`
 export default Login;
